cb7428ed4a948d4de2a49b003cea79c7b095bd7d
Sean Reilly <qwv4851@gmail.com>
2025-10-14 16:52:47 -0700

    Fix 404 deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,12 @@ export default defineConfig({
     historyApiFallback: true,
     host: true, // or '0.0.0.0'
     port: 5173
+  },
+   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
   }
 });
